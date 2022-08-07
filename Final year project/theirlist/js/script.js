21ca026c60b7171a46f3e895fefc5afb02f974d5
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2022-08-07 12:04:53 +0600

    Update script.js

--- Final year project/theirlist/js/script.js
+++ Final year project/theirlist/js/script.js
@@ -5,60 +5,53 @@ const login_btn = document.getElementById("login");
 const login_mob = document.getElementById("login_mob");
 const closee = document.getElementById("closee");
 
-login_btn.addEventListener("click",openlog);
-login_mob.addEventListener("click",openlog);
-closee.addEventListener("click",closelog);
-menu.addEventListener("click",fix);
+login_btn.addEventListener("click", openlog);
+login_mob.addEventListener("click", openlog);
+closee.addEventListener("click", closelog);
+menu.addEventListener("click", fix);
 
 function closelog() {
-    login.style.display = "none";
+  login.style.display = "none";
 }
 
 function openlog() {
-    if (login.style.display === "none") {
-      login.style.display = "flex";
-    } else {
-      login.style.display = "none";
-    }
+  if (login.style.display === "none") {
+    login.style.display = "flex";
+  } else {
+    login.style.display = "none";
   }
-
+}
 
 const signup = document.getElementById("signuppage");
 const sign_btn = document.getElementById("signup");
 const sign_mob = document.getElementById("signup_mob");
 const closeer = document.getElementById("closeer");
-sign_btn.addEventListener("click",opensin);
-sign_mob.addEventListener("click",opensin);
-closeer.addEventListener("click",closesin);
+sign_btn.addEventListener("click", opensin);
+sign_mob.addEventListener("click", opensin);
+closeer.addEventListener("click", closesin);
 
 function closesin() {
-    signup.style.display = "none";
+  signup.style.display = "none";
 }
 
 function opensin() {
-    if (signup.style.display === "none") {
-      signup.style.display = "flex";
-    } else {
-      signup.style.display = "none";
-    }
+  if (signup.style.display === "none") {
+    signup.style.display = "flex";
+  } else {
+    signup.style.display = "none";
   }
+}
 
-
-
-
-
-function fix(){
-    if(ham.style.marginTop === "-300px")
-    {
-        ham.style.marginTop = "0px";
-        ham.style.transition = "600ms ease all";
-        menu.src = "../images/menc.png";
-    }
-    else
-    {
-        ham.style.marginTop = "-300px";
-        ham.style.transition = "600ms ease all";
-        menu.src = "../images/meno.png";
-    }
-
+function fix() {
+  if (ham.style.marginTop === "-300px") {
+    ham.style.marginTop = "0px";
+    ham.style.transition = "600ms ease all";
+    menu.src = "../images/menc.png";
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  } else {
+    ham.style.marginTop = "-300px";
+    ham.style.transition = "600ms ease all";
+    menu.src = "../images/meno.png";
+  }
 }
